e96dd4c251303b7e18b43bd91527c56351e7c6ea
Chrys-Chacha <chryschacha@gmail.com>
2019-06-17 14:23:53 +0300

    all changes to form and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,123 @@
 
-// Business Logic //
-
-
-var medium = ["Medium"];
-var large = ["Large"];
-var mutton = ["Mutton"];
-var veggie = ["Vegetarian"];
-var pep = ["Pepperoni"];
-var crispy = ["Crispy"];
-var stuffed = ["Stuffed"];
-var glutten = ["Glutten-free"];
-
-function Pizza(pSize, topping, crust, price) {
-  this.pSize = pSize;
-  this.topping = topping;
-  this.crust = crust;
-  this.price = 0;
-}
-
-Pizza.prototype.addToPrice = function () {
-  if (small.includes(this.pSize)) {
-    this.price += 500;
-  }
-  if (medium.includes(this.pSize)) {
-    this.price += 1000;
-  }
-  if (large.includes(this.pSize)) {
-    this.price += 1500;
-  }
-  if (mutton.includes(this.topping)) {
-    this.price += 200;
-  }
-  if (veggie.includes(this.topping)) {
-    this.price += 100;
-  }
-  if (pep.includes(this.topping)) {
-    this.price += 150;
-  }
-  if (crispy.includes(this.crust)) {
-    this.price += 100;
-  }
-  if (stuffed.includes(this.crust)) {
-    this.price += 100;
-  }
-  if (glutten.includes(this.crust)) {
-    this.price += 50;
-  }
-};
-
-// User Interface //
-
 $(document).ready(function () {
-  $(".formInput").submit(function (event) {
-    event.preventDefault();
-    var pSize = $("#pSize").val();
-    var topping = $("#topping").val();
-    var crust = $("#crust").val();
-    var price = 0;
-
-    function invoice(){ var newPizza = new Pizza(pSize, toppings, crust, price);
-
-    newPizza.addToPrice();
-
-    console.log(newPizza);
-    $("#list").append("<ul>" + newPizza.pSize + "</ul>");
-    $("#list").append("<ul>" + newPizza.topping + "</ul>");
-    $("#list").append("<ul>" + newPizza.crust + "</ul>");
-    $("#list").append("<ul>" + "Total." + newPizza.price + "</ul>");
-
-    $(".formInput").hide();
-    $("#orderConfirmation").show();
+  $(".choices").click(function () {
+    $(this).next("div").slideToggle("slow");
   });
-  
 });
+
+function invoice() {
+
+  // Price and size
+  var size = document.querySelector('input[name="size"]:checked').value; console.log("size=" + size);
+  var sizePrice = 500;
+  if (size === "Small") {
+    sizePrice = 600;
+  };
+  if (size === "Medium") {
+    sizePrice = 1000;
+  };
+  if (size === "Large") {
+    sizePrice = 1400;
+  }; console.log("sizePrice=" + sizePrice);
+
+  // Cheese and price
+  var cheese = document.querySelector('input[name="cheese"]:checked').value; console.log("cheese=" + cheese);
+  var cheesePrice = 100;
+  if (cheese === "Extra") {
+    if (size === "Small") {
+      cheesePrice = 100;
+    } else if (size === "Medium") {
+      cheesePrice = 200;
+    } else if (size === "Large") {
+      cheesePrice = 300;
+    } else {
+      cheesePrice = 400;
+    };
+  }; console.log("cheesePrice=" + cheesePrice);
+
+  // Crust and price
+  var crust = document.querySelector('input[name="crust"]:checked').value; console.log("crust=" + crust);
+  var crustPrice = 100
+  if (crust = "Cheese-Stuffed") {
+    crustPrice = 300;
+  }; console.log("crustPrice=" + crustPrice);
+
+  // Sauce
+  var sauce = document.querySelector('input[name="sauce"]:checked').value; console.log("sauce=" + sauce);
+
+  // Meat and meat price.
+  var meatPrice = 200
+  var meat = document.getElementsByName("meat");
+  var meatSelect = [];
+  var meatDisplay = "";
+  for (var i = 0; i < meat.length; i++) {
+    if (meat[i].checked) {
+      meatSelect.push(meat[i].value);
+    };
+  }; console.log("meatSelect=" + meatSelect);
+  if (meatSelect.length === 1) {
+    meatDisplay = meatSelect[0];
+  } else if (meatSelect.length > 1) {
+    for (i = 0; i < meatSelect.length; i++) {
+      meatDisplay += meatSelect[i];
+      if (i < (meatSelect.length - 1)) {
+        meatDisplay += ", ";
+      };
+    };
+  };
+  meatPrice = meatSelect.length * 200; console.log("meatPrice=" + meatPrice);
+  if (meatSelect.length === 0) {
+    meatDisplay = "No Meats";
+  }
+
+  // Veggie and vegPrice
+  var vegPrice = 100;
+  var veg = document.getElementsByName("veg");
+  var vegSelect = [];
+  var vegDisplay = "";
+  for (var i = 0; i < veg.length; i++) {
+    if (veg[i].checked) {
+      vegSelect.push(veg[i].value);
+    };
+  }; console.log("vegSelect=" + vegSelect);
+  if (vegSelect.length > 400) {
+    vegPrice = 200;
+  }; console.log("vegPrice=" + vegPrice);
+  if (vegSelect.length === 1) {
+    vegDisplay = vegSelect[0];
+  } else if (vegSelect.length > 1) {
+    for (i = 0; i < vegSelect.length; i++) {
+      vegDisplay += vegSelect[i];
+      if (i < (vegSelect.length - 1)) {
+        vegDisplay += ", ";
+      };
+    };
+  };
+  if (vegSelect.length === 0) {
+    vegDisplay = "No Vegetables";
+  }
+
+
+  // Total price
+  var totalPrice = (vegPrice + meatPrice + crustPrice + cheesePrice + sizePrice); console.log("totalPrice=" + totalPrice);
+
+  // Invoice table
+  $("#size").html(size + " Pizza");
+  $("#sizeprice").html(" $" + sizePrice + ".00");
+  $("#crust").html(crust + " Crust");
+  $("#crustprice").html("+" + crustPrice + ".00");
+  $("#cheese").html(cheese + " Cheese");
+  $("#cheeseprice").html("+" + cheesePrice + ".00");
+  $("#sauce").html(sauce + " Sauce");
+  $("#sauceprice").html("+0.00");
+  $("#meat").html(meatDisplay);
+  $("#meatprice").html("+" + meatPrice + ".00");
+  $("#veg").html(vegDisplay);
+  $("#vegprice").html("+" + vegPrice + ".00");
+  $("#totalprice").html(" $" + totalPrice + ".00");
+};
+//Feedback after submission of delivery details
+$(".formInput").submit(function(){
+  event.preventDefault();
+  var name = $("#name").val()
+  $(".result").append("Thank you "+ name+ "your order will be delivered in an hour.");});
